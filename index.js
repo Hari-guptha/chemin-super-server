2323de33913c5dd97209a637b7a1ffc393c57a8a
Hari_Guptha <80567787+Hari-guptha@users.noreply.github.com>
2023-03-25 11:20:03 +0530

    3

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("views"))
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.render("index.ejs")
 })
 
+app.get('/api', (req, res) => {
+    res.render("po da venna")
+})
 app.post('/home', (req, res) => {
     res.send("we are good")
 })
